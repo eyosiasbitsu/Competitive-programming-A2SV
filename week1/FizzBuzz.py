9baa55708bf80f3c555243b9e16614c6360504ee
Fetsum Abyu <85545252+eyosiasbitsu@users.noreply.github.com>
2021-12-10 22:53:07 +0300

    Update FizzBuzz.py

--- week1/FizzBuzz.py
+++ week1/FizzBuzz.py
@@ -1,39 +1,13 @@
-import math
-import os
-import random
-import re
-import sys
-
-#
-# Complete the 'insertionSort1' function below.
-#
-# The function accepts following parameters:
-#  1. INTEGER n
-#  2. INTEGER_ARRAY arr
-#
-
-def insertionSort1(n, arr):
-        min_value = arr[n-1]
-        for i in range(n-2,-1,-1):
-            if arr[i] > min_value:
-                arr[i+1] = arr[i]
-                print(' '.join(map(str,arr)))
-            else :
-                arr[i+1] = min_value
-                print(' '.join(map(str,arr)))
-            
-                break
-            if i == 0:
-                if arr[i] < min_value:
-                    arr[i+1] = min_value
-                    print(' '.join(map(str,arr)))
-                else:
-                    arr[i] = min_value
-                    print(' '.join(map(str,arr)))        
-            
-if __name__ == '__main__':
-    n = int(input().strip())
-
-    arr = list(map(int, input().rstrip().split()))
-
-    insertionSort1(n, arr)
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        result = []
+        for i in range(1,n+1):
+            if i%15 == 0:
+                result.append("FizzBuzz")
+            elif i%5 == 0:
+                result.append("Buzz")
+            elif i%3 == 0:
+                result.append("Fizz")
+            else:
+                result.append(str(i))
+        return result
