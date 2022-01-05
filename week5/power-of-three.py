108d8cd4c1ea77f1227c67b9134eb9511de61079
Fetsum Abyu <eyosiasbitsu@gmail.com>
2022-01-05 10:36:31 +0300

    created two new files and week5 folder

---
+++ week5/power-of-three.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n == 1:
+            return True
+        if n < 1:
+            return False
+        else:
+            return (n%3 == 0) and (self.isPowerOfThree(n//3))
