976ddc935be928aa6a44ce9c444a31c6c0281141
Fetsum Abyu <85545252+eyosiasbitsu@users.noreply.github.com>
2022-04-06 22:41:33 +0300

    Time: 848 ms (64.23%), Space: 20.3 MB (7.49%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.py
+++ 630-course-schedule-iii/630-course-schedule-iii.py
@@ -6,7 +6,6 @@ class Solution:
         heap = []
         
         for el in courses:
-            print(el[0],el[1])
             if time + el[0] <= el[1]:
                 heapq.heappush(heap,-1*el[0])
                 time += el[0]
@@ -19,17 +18,3 @@ class Solution:
                         heapq.heappush(heap,-1*el[0])
                     
         return len(heap)
-    
-    
-# 		for ele in courses:
-# 			if ele[0]+time<=ele[1]:
-# 				heappush(h,-ele[0])
-# 				time+=ele[0]
-
-# 			else:
-# 				if h:
-# 					if ele[0]<-h[0]:
-# 						time-=-(heappop(h))
-# 						time+=ele[0]
-# 						heappush(h,-ele[0])
-        
