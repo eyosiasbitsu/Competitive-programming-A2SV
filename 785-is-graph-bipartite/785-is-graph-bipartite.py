2bdae39f595ae2860feaa10c0d23f18717b3e877
Fetsum Abyu <85545252+eyosiasbitsu@users.noreply.github.com>
2022-04-30 00:22:32 +0300

    Time: 306 ms (23.29%), Space: 14.6 MB (37.31%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.py
+++ 785-is-graph-bipartite/785-is-graph-bipartite.py
@@ -1,18 +1,39 @@
+class disjoint:
+    def __init__(self,size):
+        self.par = [i for i in range(size)]
+        self.rank = [1 for i in range(size)]
+        
+    def find(self,x):
+        if self.par[x] == x:
+            return x
+        self.par[x] = self.find(self.par[x])
+        
+        return self.par[x]
+    
+    def union(self,x,y):
+        parx = self.find(x)
+        pary = self.find(y)
+        if parx != pary:
+            if self.rank[pary] > self.rank[parx]:
+                self.par[parx] = pary
+            elif self.rank[pary] < self.rank[parx]:
+                self.par[pary] = parx
+            else:
+                self.par[pary] = parx
+                self.rank[parx] += 1
+                
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
-        color = [0]*len(graph)
+        disj = disjoint(len(graph))
         
         for i in range(len(graph)):
-            if color[i] == 0 and not self.validColor(graph,color,1,i):return False
+            for j in graph[i]:
+                
+                t1 = disj.find(i)
+                t2 = disj.find(j)
+                print(j,graph[i][0])
+                if t1 == t2:
+                    return False
+                disj.union(j,graph[i][0])
+             
         return True
-    def validColor(self,graph,color,curr,node):
-        if color[node] != 0:
-            return color[node] == curr
-        
-        color[node] = curr
-        
-        for n in graph[node]:
-            if not self.validColor(graph,color,-curr,n): return False
-        return True
-        
-        
