42b2655f53e67667085190e18dbc3b83ed46f8d0
Fetsum Abyu <85545252+eyosiasbitsu@users.noreply.github.com>
2022-05-01 00:08:38 +0300

    Time: 87 ms (11.77%), Space: 14.3 MB (90.81%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.py
@@ -1,39 +1,32 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        
-        
         l = 0
         r = len(matrix) - 1
-        def inrange(n,m,o):
-            if n <= o and o<= m:
-                return True
-            else:
-                return False
+        if target < matrix[0][0] or target > matrix[-1][-1]:
+            return False
+        
         while l <= r:
-            mid1 = (l + r)//2
-            # print([l,mid1])
-            if inrange(matrix[mid1][0],matrix[mid1][len(matrix[mid1])-1],target):
-                templ = 0
-                tempr = len(matrix[mid1])-1
-                while templ <= tempr:
-                    mid2 = templ + (tempr - templ)//2
-                    if matrix[mid1][mid2] < target:
-                        templ = mid2 + 1
-                    elif matrix[mid1][mid2] > target:
-                        tempr = mid2 - 1
-                    elif matrix[mid1][mid2] == target:
-                        return True
-                    elif l == r and matrix[mid1][mid2] != target:
-                        return False
-                # print([templ,mid1])
-                return False
-            elif matrix[mid1][0] > target:
-                r = mid1 - 1
-            elif matrix[mid1][0] < target:
-                l = mid1 + 1
-            elif l == r and not inrange(matrix[mid1][0],matrix[mid1][len(matrix[mid1])-1],target):
-                return False
-        return False
-        
+            mid = (l+r)//2
+            if matrix[mid][0] <= target <= matrix[mid][-1]:
+                return self.bs(0,len(matrix[mid])-1,matrix[mid],target)
             
+            elif matrix[mid][0] < target:
+                l = mid + 1
+                
+            elif matrix[mid][0] > target:
+                r = mid - 1
         
+    def bs(self,l,r,arr,target):
+        
+        while l <= r:
+            mid = (l+r)//2
+
+            if arr[mid] < target:
+                l = mid + 1
+            elif arr[mid] > target:
+                r = mid - 1
+            else:
+                return True
+            
+        return False
+                    
