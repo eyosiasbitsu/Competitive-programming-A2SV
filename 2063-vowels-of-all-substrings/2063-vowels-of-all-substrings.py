a00072b22a724a526b3fcea657e3329ea31c633a
Fetsum Abyu <85545252+eyosiasbitsu@users.noreply.github.com>
2022-05-01 15:56:34 +0300

    Time: 135 ms (81.27%), Space: 14.9 MB (45.12%) - LeetHub

--- 2063-vowels-of-all-substrings/2063-vowels-of-all-substrings.py
+++ 2063-vowels-of-all-substrings/2063-vowels-of-all-substrings.py
@@ -6,6 +6,5 @@ class Solution:
         for i in range(len(word)):
             if word[i] in "aeiou":
                 count += ((i+1)*(n-i))
-                print(i+1,n-i,word[i])
         
         return count
