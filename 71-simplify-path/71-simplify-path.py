881c30343ceedf0e2a4b21b246b05ca14ac91315
Fetsum Abyu <85545252+eyosiasbitsu@users.noreply.github.com>
2022-05-08 02:08:40 +0300

    Time: 41 ms (67.77%), Space: 13.8 MB (82.97%) - LeetHub

---
+++ 71-simplify-path/71-simplify-path.py
@@ -0,0 +1,17 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        stk = []
+        cur = ""
+        
+        for c in path + "/":
+            if c == "/":
+                if cur == '..':
+                    if stk: stk.pop()
+                elif cur != "" and cur != '.':
+                    stk.append(cur)
+                cur = ""
+                
+            else:
+                cur += c
+        
+        return '/'+"/".join(stk)
