18cd92444ece9c69ddc0b15b9b0f181f52d1b6f0
Fetsum Abyu <85545252+eyosiasbitsu@users.noreply.github.com>
2022-05-09 18:14:58 +0300

    Time: 55 ms (18.81%), Space: 13.9 MB (31.84%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -1,20 +1,27 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
+        # if the digits string is empty then return empty list since there will
+        # not be any string combinations
+        
         if not digits:
             return []
+        # inorder to know the combination of letters we must first set
+        # the relationship between the numbers and the letters, to do this
+        # what better datastructure is there other than a dictionary
         
         self.dict1 = {"2" : "abc","3":"def", "4":"ghi", "5":"jkl", "6":"mno","7":"pqrs","8":"tuv","9":"wxyz"}
-        self.res = []
+        self.res = [] # this is the array we are supposed to return (we'll populate it later)
         
-        def dp(i,str1):
+        #
+        def dfs(i,str1):
             if i == len(digits):
                 self.res.append(str1)        
                 return 
             
             for c in self.dict1[digits[i]]:
-                dp(i+1,str1 + c)
+                dfs(i+1,str1 + c)
         
-        dp(0,"")
+        dfs(0,"")
         
         return self.res
                 
