6203470c5e6ae45d6f9cec63fd135a4e5a7489cb
Fetsum Abyu <85545252+eyosiasbitsu@users.noreply.github.com>
2022-05-16 18:11:19 +0300

    Time: 1077 ms (23.48%), Space: 14.2 MB (95.32%) - LeetHub

---
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -0,0 +1,32 @@
+class Solution:
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        n = len(grid[0])
+        dirs = [(0,1),(1,0),(1,1),(-1,-1),(-1,0),(0,-1),(-1,1),(1,-1)]
+        if grid[0][0] != 0:
+            return -1
+        
+        cach = {}
+        
+        que = deque()
+        que.append((0,0,1))
+        step = float("inf")
+        grid[0][0] = 1
+        
+        while que:
+            i,j,st = que.popleft()
+            
+            if (i,j) == (n-1,n-1):
+                step = min(step, st)
+                continue
+            
+            for i_add,j_add in dirs:
+                new_i = i + i_add
+                new_j = j + j_add
+                
+                if(0<= new_i< n and 
+                   0 <= new_j < n and 
+                   grid[new_i][new_j] == 0):
+                    que.append((new_i,new_j,st+1))
+                    grid[new_i][new_j] = 1
+        
+        return step if step != float("inf") else -1
