2f2d2d66f14d0b8f19b177f88d1e5053db0f878a
Fetsum Abyu <85545252+eyosiasbitsu@users.noreply.github.com>
2022-06-05 10:08:32 +0300

    Time: 53 ms (83.10%), Space: 13.8 MB (99.25%) - LeetHub

--- 52-n-queens-ii/52-n-queens-ii.py
+++ 52-n-queens-ii/52-n-queens-ii.py
@@ -1,7 +1,5 @@
 class Solution:
     def totalNQueens(self, n: int) -> int:
-        board = [["."]*n for _ in range(n)]
-        
         col = set()
         posD = set()
         negD = set()
@@ -9,21 +7,19 @@ class Solution:
         self.count = 0
         
         def backtrack(r):
-            
             if r == n:
                 self.count += 1
                 return
             
             for c in range(n):
-                if (c in col or r - c in negD or
-                    r + c in posD):
+                if c in col or r-c in negD or c + r in posD:
                     continue
                 
                 col.add(c)
                 posD.add(r + c)
                 negD.add(r - c)
                 
-                backtrack(r+1)
+                backtrack(r + 1)
                 
                 col.remove(c)
                 posD.remove(r + c)
@@ -31,6 +27,3 @@ class Solution:
         
         backtrack(0)
         return self.count
-                
-                
-                
