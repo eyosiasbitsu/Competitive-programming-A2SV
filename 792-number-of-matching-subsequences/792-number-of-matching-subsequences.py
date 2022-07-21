df41f486b098878199ae0f97c7f2d9d1849c3b43
Fetsum Abyu <85545252+eyosiasbitsu@users.noreply.github.com>
2022-07-21 11:01:33 +0300

    Time: 1308 ms (25.82%), Space: 17.4 MB (16.33%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.py
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.py
@@ -1,41 +1,39 @@
 class Solution:
     def numMatchingSubseq(self, s: str, words: List[str]) -> int:
-        lookup = defaultdict(list)
+        
+        indexes = defaultdict(list)
         res = 0
         
         for i,c in enumerate(s):
-            lookup[c].append(i)
+            indexes[c].append(i)
+        
+        def bs(ar,i):
+            l,r = 0,len(ar)
             
-        def bs(lst,i):
-            l,r = 0,len(lst)
             while l < r:
                 mid = (l + r)//2
-                if i < lst[mid]:
+                
+                if i < ar[mid]:
                     r = mid
                 
                 else:
                     l = mid + 1
-                    
+            
             return l
         
         for w in words:
             prev = -1
             found = True
             for c in w:
-                temp = bs(lookup[c], prev)
-                if temp == len(lookup[c]):
+                temp = bs(indexes[c], prev)
+                if temp == len(indexes[c]):
                     found = False
                     break
                 
                 else:
-                    prev = lookup[c][temp]
+                    prev = indexes[c][temp]
             
             if found:
                 res += 1
         
         return res
-#         create a hash
-
-#         binary search
-
-#         return
