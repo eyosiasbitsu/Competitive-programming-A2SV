c331604467184fe68f02484a52fced3f99450d77
Fetsum Abyu <85545252+eyosiasbitsu@users.noreply.github.com>
2022-07-27 01:10:15 +0300

    Time: 70 ms (48.76%), Space: 14.1 MB (70.62%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.py
+++ 97-interleaving-string/97-interleaving-string.py
@@ -3,22 +3,16 @@ class Solution:
         
         if len(s1) + len(s2) != len(s3):
             return False
-        dp = {}
         
-        def dfs(i,j):
-            if i == len(s1) and j == len(s2):
-                return True
-            
-            if (i,j) in dp:
-                return dp[(i,j)]
-            
-            if i < len(s1) and s1[i] == s3[i + j] and dfs(i + 1, j):
-                return True
-            
-            if j < len(s2) and s2[j] == s3[i + j] and dfs(i, j + 1):
-                return True
-            
-            dp[(i,j)] = False
-            return False
+        dp = [[False]*(len(s2) + 1) for _ in range(len(s1) + 1)]
+        dp[len(s1)][len(s2)] = True
         
-        return dfs(0,0)
+        for i in range(len(s1), -1, -1):
+            for j in range(len(s2), -1, -1):
+                if i < len(s1) and s1[i] == s3[i + j] and dp[i + 1][j]:
+                    dp[i][j] = True
+                
+                if j < len(s2) and s2[j] == s3[i + j] and dp[i][j + 1]:
+                    dp[i][j] = True
+                
+        return dp[0][0]
