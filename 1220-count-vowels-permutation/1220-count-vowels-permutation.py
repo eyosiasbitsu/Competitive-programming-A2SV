3d2906d3019f4c39bda2106b2c6ad5517057ac10
Fetsum Abyu <85545252+eyosiasbitsu@users.noreply.github.com>
2022-08-08 03:14:20 +0300

    Time: 229 ms (86.74%), Space: 13.9 MB (85.82%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.py
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.py
@@ -1,19 +1,19 @@
 class Solution:
     def countVowelPermutation(self, n: int) -> int:
-        v_dic = {'a': ['e'], 'e': ['a', 'i'], 'i':['a', 'e', 'o', 'u'],
-                'o':['i', 'u'], 'u':['a']}
-        @cache
-        def search(n, v):
-            num = 0
-            if n == 1:
-                return 1
-            next_v_lst = v_dic[v]
-            for next_v in next_v_lst:
-                num += search(n-1, next_v)
-            return num
-        return sum([search(n, v) for v in v_dic]) % (10**9+7)
-    
-    
-    
-    
-    
+        
+        dp = [1,1,1,1,1]
+        
+        a,e,i,o,u = 0, 1, 2, 3, 4
+        
+        for j in range(2, n + 1):
+            temp = [0, 0, 0, 0, 0]
+            
+            temp[a] = (dp[e] + dp[i] + dp[u])% (10**9+7)
+            temp[e] = (dp[a] + dp[i])% (10**9+7)
+            temp[i] = (dp[e] + dp[o])% (10**9+7)
+            temp[o] = dp[i]
+            temp[u] = dp[i] + dp[o]
+            
+            dp = temp
+        
+        return sum(dp)% (10**9+7)
