3c6d1c95b6cde844006ad9783109bd4334a5ef01
Fetsum Abyu <85545252+eyosiasbitsu@users.noreply.github.com>
2022-08-19 05:03:11 +0300

    Time: 151 ms (46.65%), Space: 21 MB (30.64%) - LeetHub

---
+++ 338-counting-bits/338-counting-bits.py
@@ -0,0 +1,9 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        
+        res = [0]
+        
+        for i in range(1,n + 1):
+            res.append(bin(i)[2:].count('1'))
+        
+        return res
