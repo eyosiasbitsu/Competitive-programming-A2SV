337cae2e1c59de107f86b90412c71c6fe095c15a
Fetsum Abyu <85545252+eyosiasbitsu@users.noreply.github.com>
2022-09-27 19:51:18 +0300

    Time: 55 ms (34.74%), Space: 13.9 MB (11.82%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,16 +1,12 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         
-        f = 0
         fv = 1
-        s = 1
         sv = 1
         
         for i in range(2, n + 1):
             temp = fv + sv
-            f = s
             fv = sv
-            s = i
             sv = temp
             
         return sv
