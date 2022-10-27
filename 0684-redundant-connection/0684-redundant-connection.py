1d678fde9f69690209a3df8bae48a2c2b56646f8
Fetsum Abyu <85545252+eyosiasbitsu@users.noreply.github.com>
2022-10-27 14:33:39 +0300

    Time: 86 ms (75.81%), Space: 14.4 MB (56.09%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -0,0 +1,55 @@
+class Disjoint:
+    
+    def __init__(self, amount):
+        self.size = [1 for _ in range(amount)]
+        self.parent = [i + 1 for i in range(amount)]
+    
+    def find(self, x):
+        
+        if x != self.parent[x - 1]:
+            self.parent[x - 1] = self.find(self.parent[x - 1])
+            
+        return self.parent[x - 1]
+    
+    def union(self, x, y):
+        x = self.find(x)
+        y = self.find(y)
+        
+        if self.same(x, y):
+            return False
+        
+        if self.size[x - 1] > self.size[y - 1]:
+            self.parent[y - 1] = x
+            self.size[x - 1] + self.size[y - 1]
+        else:
+            self.parent[x - 1] = y
+            self.size[y - 1] += self.size[x - 1]        
+        
+        return True
+    
+    def same(self, x, y):
+        return self.parent[x - 1] == self.parent[y - 1]
+
+    
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        
+        size = 0
+        
+        for fr, to in edges:
+            size = max(size, fr , to)
+            
+        unionFind = Disjoint(size)
+        for fr, to in edges:
+            if not unionFind.union(fr, to):
+                return [fr, to]
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
