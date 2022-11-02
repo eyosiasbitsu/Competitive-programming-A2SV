eb8c13eac587f2693c5020e6104cfd4d04fe8fdb
Fetsum Abyu <85545252+eyosiasbitsu@users.noreply.github.com>
2022-11-02 10:31:29 +0300

    Time: 365 ms (27.67%), Space: 15.2 MB (80.75%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,10 +1,11 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         
-        nums.sort()
+        _xor = 0
+        for n in nums:
+            _xor ^= n
         
-        for i in range(1, len(nums)):
-            if nums[i] - nums[i - 1] != 1:
-                return nums[i] - 1
-            
-        return len(nums) if nums[0] == 0 else 0
+        for i in range(len(nums) + 1):
+            _xor ^= i
+        
+        return _xor
