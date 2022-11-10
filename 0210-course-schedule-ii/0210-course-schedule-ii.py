e6faa2ad0ac150d391855799d816654b0d80619f
Fetsum Abyu <85545252+eyosiasbitsu@users.noreply.github.com>
2022-11-11 01:29:01 +0300

    Time: 231 ms (46.98%), Space: 15.4 MB (88.07%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -0,0 +1,35 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        
+        adj = [[] for _ in range(numCourses)]
+        indegree = [0 for _ in range(numCourses)]
+        topSort = []
+        
+        for dest, src in prerequisites:
+            adj[src].append(dest)
+            indegree[dest] += 1
+        
+        queue = deque([])
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                queue.append(i)
+        
+        while queue:
+            cur = queue.popleft()
+            topSort.append(cur)
+            
+            for child in adj[cur]:
+                indegree[child] -= 1
+                
+                if indegree[child] == 0:
+                    queue.append(child)
+        
+        if len(topSort) == numCourses:
+            return topSort
+        
+        return []
+                              
+                              
+                              
+                              
+                              
