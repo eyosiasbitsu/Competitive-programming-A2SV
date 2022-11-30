a3309fc37cc09e222d22ec4945e91cb5b40e2e8f
Fetsum Abyu <85545252+eyosiasbitsu@users.noreply.github.com>
2022-11-30 13:58:02 +0300

    Time: 963 ms (95.11%), Space: 32.2 MB (99.92%) - LeetHub

---
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.py
@@ -0,0 +1,18 @@
+class Solution:
+    def garbageCollection(self, garbage: List[str], travel: List[int]) -> int:
+        
+        _set = set()
+        count = 0
+        
+        for st in garbage:
+            count += len(st)
+            
+        for i in range(len(garbage) - 1, -1, -1):
+            _set |= set(garbage[i])
+            garbage[i] = len(_set)
+        
+        result = 0
+        for i in range(len(travel)):
+            result += ((travel[i])*garbage[i + 1])
+        
+        return result + count
