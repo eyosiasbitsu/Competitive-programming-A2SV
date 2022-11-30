86457f1d1dca64b58f1d929dd85c5a6f29a73e4a
Fetsum Abyu <85545252+eyosiasbitsu@users.noreply.github.com>
2022-11-30 14:43:27 +0300

    Time: 611 ms (15.48%), Space: 14.5 MB (54.79%) - LeetHub

--- 0654-maximum-binary-tree/0654-maximum-binary-tree.py
+++ 0654-maximum-binary-tree/0654-maximum-binary-tree.py
@@ -7,28 +7,25 @@
 class Solution:
     def constructMaximumBinaryTree(self, nums: List[int]) -> Optional[TreeNode]:
         
-        tree = self.dfs(nums, 0, len(nums) - 1)
+        tree = self.dfs(nums, 0, len(nums))
         
         return tree
     
     def dfs(self, nums, left, right):
-        if right < left:
-            return None
-        
         if right == left:
-            return TreeNode(nums[right])
+            return None
         
         node = TreeNode()
         idx = -1
         val = -1
         
-        for i in range(left, right + 1):
+        for i in range(left, right):
             if nums[i] > val:
                 idx = i
                 val = nums[i]
                 
         node.val = val
-        node.left = self.dfs(nums, left, idx - 1)
+        node.left = self.dfs(nums, left, idx)
         node.right = self.dfs(nums, idx + 1, right)
         
         return node
