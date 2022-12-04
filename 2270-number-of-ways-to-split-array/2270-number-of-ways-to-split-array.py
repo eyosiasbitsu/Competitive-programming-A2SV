6bb61b38bec409b0f17c5e69246ac8834bd3526c
Fetsum Abyu <85545252+eyosiasbitsu@users.noreply.github.com>
2022-12-05 01:58:52 +0300

    Time: 1085 ms (83.01%), Space: 29.3 MB (21.68%) - LeetHub

---
+++ 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.py
@@ -0,0 +1,16 @@
+class Solution:
+    def waysToSplitArray(self, nums: List[int]) -> int:
+        
+        _sum = 0
+        
+        for i in range(len(nums)):
+            _sum += nums[i]
+            nums[i] = _sum
+        
+        num_ways = 0
+        
+        for i in range(len(nums)):
+            if i != len(nums) - 1 and nums[-1] - nums[i] <= nums[i]:
+                num_ways += 1
+        
+        return num_ways
