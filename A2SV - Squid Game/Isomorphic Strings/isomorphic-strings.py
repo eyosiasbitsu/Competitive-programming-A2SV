26f6c6b872fdda9058dffcf1f27afa447efdc56e
Fetsum Abyu <85545252+eyosiasbitsu@users.noreply.github.com>
2023-02-23 00:04:23 +0300

    Create isomorphic-strings.py

---
+++ A2SV - Squid Game/Isomorphic Strings/isomorphic-strings.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+
+        mp_s_t = {}
+        mp_t_s = {}
+        
+        for i in range(len(s)):
+            if t[i] in mp_t_s:
+                if s[i] != mp_t_s[t[i]]:
+                    return False
+
+                continue
+            
+            if s[i] in mp_s_t:
+                if t[i] != mp_s_t[s[i]]:
+                    return False
+                    
+                continue
+                
+            mp_t_s[t[i]] = s[i]
+            mp_s_t[s[i]] = t[i]
+        
+        return True
